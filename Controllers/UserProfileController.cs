ece58646e3c75c85873b57440f6f3eb53fde8f35
Reinir <blackwhitenxt@gmail.com>
2023-09-20 15:09:38 +0700

    Static file linking fix

---
+++ Controllers/UserProfileController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace forum.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        [Route("profile")]
+        public IActionResult UserProfile()
+        {
+            return View("UserProfile");
+        }
+    }
+}
