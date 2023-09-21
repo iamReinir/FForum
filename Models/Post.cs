075749bf5c294b23dcdaec5f8f1431bb546f82ea
Reinir <blackwhitenxt@gmail.com>
2023-09-21 15:55:35 +0700

    UserSet Register and getUser

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace forum.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public User Poster { get; }
+        public bool Is_hidden {  get; set; } = false;
+
+        public PostInfo info { get; }
+        public DateTime Last_update { get; } = DateTime.Now;
+
+        public DateTime Create_date { get; } = DateTime.Now;
+
+    }
+}
