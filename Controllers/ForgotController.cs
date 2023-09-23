ad825377e2480ab34be67e810b7cd4aaa676217c
HT9303 <lenovo@THINKPAD>
2023-09-23 21:39:35 +0700

    Update Forgot and resetpass page

---
+++ Controllers/ForgotController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace forum.Controllers
+{
+	public class ForgotController : Controller
+	{
+		[Route("Forgot")]
+		public IActionResult Forgot()
+		{
+			return View("Forgot");
+		}
+		[Route("ResetPass")]
+		public IActionResult ResetPass()
+		{
+			return View("ResetPass");
+		}
+	}
+}
