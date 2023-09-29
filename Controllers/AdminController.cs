9d57e2f32b0c07b4d1099568f79c4e2a9f8e7b7a
84702 <84702@DONGLOI>
2023-09-29 22:15:10 +0700

    If you like my design you can merge it

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using forum.Database;
+using forum.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace forum.Controllers
+{
+    public class AdminController:Controller
+    {
+        [Route("/admin_page")]
+        public IActionResult Index()
+        {
+            ISession session = HttpContext.Session;
+            UserSet uset = new UserSet();
+
+            string? username = session.GetString("username");
+            var user = uset.GetUser(username);
+            return View("Admin_page", user);
+        }
+    }
+}
+
