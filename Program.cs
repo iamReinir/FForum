3109940f74683956434877dba7cf8cd1d87de25e
Reinir <blackwhitenxt@gmail.com>
2023-09-30 07:03:19 +0700

    remove test code

--- Program.cs
+++ Program.cs
@@ -18,17 +18,6 @@ var app = builder.Build();
 app.UseSession();
 app.MapControllers();
 app.UseStaticFiles();
-
-
-var uset = new UserSet();
-var pset = new PostSet();
-User? user = uset.GetUser("reinir2");
-Post p = pset.NewPost(user);
-var pinfo = pset.GetPostInfo(p);
-pinfo.Content = "Good evening, even tho its 8am rn...";
-pset.UpdatePostInfo(pinfo);
-
-
 app.Run();
 
 
