81cf1ecdb0bd24f04443bcaf18233b00554446e4
HuyBackTracking <huybacktracking2206@gmail.com>
2023-10-03 10:33:55 +0700

    pull

---
+++ Controllers/PostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace forum.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
