3ef4523892ef4f765fd3b8a77753e5326246accb
Reinir <blackwhitenxt@gmail.com>
2023-10-11 16:10:26 +0700

    Wait for connection

--- Database/MongoDB.cs
+++ Database/MongoDB.cs
@@ -6,12 +6,27 @@ namespace forum.Database
     static public class MongoDB
     {
         public const string connection_string = "mongodb+srv://group3:group3@cluster0.1rrhgmx.mongodb.net/?retryWrites=true&w=majority";
-        public static MongoClient dbClient = new MongoClient(connection_string);
+        public static MongoClient dbClient;
         public const string dbName = "fforum_db";
-        public static IMongoDatabase database = dbClient.GetDatabase(MongoDB.dbName);
+        public static IMongoDatabase database;
         public const string USER_TABLE = "user";
         public const string USER_INFO_TABLE = "user_info";
         public const string POST_TABLE = "post";
         public const string POST_INFO_TABLE = "post_info";
+        static MongoDB()
+        {
+            Console.WriteLine("Connect to database...");
+            while (database == null)
+            { 
+                try {
+                    dbClient = new MongoClient(connection_string);
+                    database = dbClient.GetDatabase(dbName);
+                }
+                catch { Console.WriteLine("Connect failed. Retry..."); }
+            }
+            Console.WriteLine("Connect success!");
+        }
+
+
     }
 }
