a2b479fe747868a74c87ed430991979f2483b459
84702 <84702@DONGLOI>
2023-10-18 15:29:52 +0700

    xin loi anh trung

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -7,11 +7,18 @@ namespace forum.Models
         [BsonElement("_id")]
         public int Id { get; set; }
         public User User { get; set; } 
-        public Post Post { get; set; }
+        public int PostId { get; set; }
         public string Content { get; set; }
         public DateTime Create_date { get; set; } = DateTime.Now;
         public DateTime Last_update { get; set; } = DateTime.Now;
         public bool Is_hidden { get; set; } = false;
 
+        public Comment() { }
+        public Comment(int id, User username, int postId)
+        {
+            Id = id;
+            User = username;
+            PostId = postId;
+        }
     }
 }
